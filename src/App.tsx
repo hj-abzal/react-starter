317801c01a3567088cf4ad7dcf79b9504e4b936d
abzal <abzal008@mail.ru>
2025-05-09 19:32:27 +0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import {Button} from "@/components/ui/button.tsx";
+
+function App() {
+
+    return (
+        <>
+            <Button variant={'default'}>starter</Button>
+        </>
+    )
+}
+
+export default App
